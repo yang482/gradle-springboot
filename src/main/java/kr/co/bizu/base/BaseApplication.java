3b73ae2f287e48433f0ba500ae4eb79c13bb4513
S.H.YANG <yang482@gmail.com>
2021-11-09 18:39:08 +0900

    Init project base.

---
+++ src/main/java/kr/co/bizu/base/BaseApplication.java
@@ -0,0 +1,16 @@
+package kr.co.bizu.base;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class BaseApplication {
+    
+    public static void main(String[] args) {
+        
+        SpringApplication.run(BaseApplication.class, args);
+    }
+}
