bd0fe4b07d42bf4e1552bc444bf23072335ff0eb
S.H.YANG <yang482@gmail.com>
2021-12-02 16:59:24 +0900

    init jdbc for mariadb.

--- src/test/java/kr/co/bizu/base/BaseApplicationTests.java
+++ src/test/java/kr/co/bizu/base/BaseApplicationTests.java
@@ -1,13 +1,26 @@
 package kr.co.bizu.base;
 
+import javax.sql.DataSource;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootTest
 class BaseApplicationTests {
 
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+    
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void jdbcTests() {
+        System.out.println(jdbcTemplate);
+    
+        jdbcTemplate.queryForMap("select 1 a");
+    }
 }
